d6087cb54957a6a0ba27fe15a8075ae6e72b2960
Catalin Muresan <muresan@pythian.com>
2017-08-29 17:40:30 +0100

    Added some pretty json stuff

--- app/server.js
+++ app/server.js
@@ -7,8 +7,11 @@ var app = express();
 
 var localip = ni['eth0'][0].address;
 
+app.enable('trust proxy');
+
 app.get('/', function(req, res) {
-  res.send("Hello v1.0 " + req.ip + " from " + localip + "\n");
+  var body = JSON.stringify({ method: req.method, url: req.url, headers: req.headers, ip: req.ip, ips: req.ips }, null, 4);
+  res.send("Hello v1.0 " + req.ip + " from " + localip + "\n" + body);
 });
 
 app.listen(3000);
